26708518b756c1bbe116bd3571bd3133c8831cc5
QuincieBird <114311657+QuincieBird@users.noreply.github.com>
2023-10-24 12:56:03 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -40,3 +40,26 @@ function loadGame() {
         console.error("Failed to load the game:", error);
     }
 }
+// Function to open the Profile window
+function openProfileWindow() {
+    const profileWindow = document.getElementById("profile-window");
+    profileWindow.style.display = "block";
+}
+
+// Function to close the Profile window
+function closeProfileWindow() {
+    const profileWindow = document.getElementById("profile-window");
+    profileWindow.style.display = "none";
+}
+
+// Function to open the Settings window
+function openSettingsWindow() {
+    const settingsWindow = document.getElementById("settings-window");
+    settingsWindow.style.display = "block";
+}
+
+// Function to close the Settings window
+function closeSettingsWindow() {
+    const settingsWindow = document.getElementById("settings-window");
+    settingsWindow.style.display = "none";
+}
